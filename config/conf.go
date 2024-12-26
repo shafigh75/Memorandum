d6bc4b210b0c08d4bc335db88c8c21bfa0e6a428
shafigh75 <shafigh75@gmail.com>
2024-12-26 09:41:40 +0330

    first phase of the project

---
+++ config/conf.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+    "encoding/json"
+    "io/ioutil"
+    "os"
+)
+
+// Config holds the configuration settings.
+type Config struct {
+    HTTPPort        string `json:"http_port"`
+    RPCPort         string `json:"rpc_port"`
+    CleanupInterval int64  `json:"cleanup_interval"` // in seconds
+    AuthEnabled     bool   `json:"auth_enabled"`
+    AuthToken       string `json:"auth_token"` // Token for authentication
+}
+
+// LoadConfig reads the configuration from a JSON file.
+func LoadConfig(filePath string) (*Config, error) {
+    file, err := os.Open(filePath)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    byteValue, _ := ioutil.ReadAll(file)
+    var config Config
+    if err := json.Unmarshal(byteValue, &config); err != nil {
+        return nil, err
+    }
+    return &config, nil
+}
+
---
+++ go.mod
@@ -0,0 +1,14 @@
+module db
+
+go 1.23.1
+
+require (
+	github.com/chzyer/readline v1.5.1
+	github.com/spf13/cobra v1.8.1
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
+)
